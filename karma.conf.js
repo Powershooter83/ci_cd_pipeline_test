3d4e5bc14ba8b478f1b68de34515eb99cd86b79f
p20102 <yanicksilvan.romere@css.ch>
2022-12-22 14:49:57 +0100

    initial 4.17

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Phantom'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
